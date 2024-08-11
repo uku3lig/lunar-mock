8877841e2d201347372e9ebd7556a578f39162bc
uku <hi@uku.moe>
2024-08-12 00:17:27 +0200

    feat: add a class

---
+++ src/main/java/com/moonsworth/lunar/HIHIHCIIIOCRHCCHIRRHCCCORCHCOR/RCOOOHCHIHHCCOOOIIHIOOHHHHOOHR/OHHORCCCROOCOCCOORIHIOORCHIHCR/OHHORCCCROOCOCCOORIHIOORCHIHCR/HHIICOHIRIIOIHRICRHIHICIIOIOCR/HHIICOHIRIIOIHRICRHIHICIIOIOCR.java
@@ -0,0 +1,21 @@
+package com.moonsworth.lunar.HIHIHCIIIOCRHCCHIRRHCCCORCHCOR.RCOOOHCHIHHCCOOOIIHIOOHHHHOOHR.OHHORCCCROOCOCCOORIHIOORCHIHCR.OHHORCCCROOCOCCOORIHIOORCHIHCR.HHIICOHIRIIOIHRICRHIHICIIOIOCR;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.kyori.adventure.text.Component;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.Entity;
+
+import java.util.List;
+
+public class HHIICOHIRIIOIHRICRHIHICIIOIOCR {
+    public static <T extends Entity> void HHIICOHIRIIOIHRICRHIHICIIOIOCR(Font textRenderer, EntityRenderDispatcher dispatcher, T entity, PoseStack matrices, MultiBufferSource vertexConsumers, int light, List<Component> lines) {
+        // sample call to be used as target in a mixin
+        HHIICOHIRIIOIHRICRHIHICIIOIOCR(textRenderer, dispatcher, entity, null, 0, matrices, vertexConsumers, light);
+    }
+
+    private static <T extends Entity> void HHIICOHIRIIOIHRICRHIHICIIOIOCR(Font textRenderer, EntityRenderDispatcher dispatcher, T entity, Component text, double y, PoseStack matrices, MultiBufferSource vertexConsumers, int light) {
+        // empty
+    }
+}
